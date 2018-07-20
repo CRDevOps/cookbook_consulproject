f296feace66b7f61e8d2a4ff524a39df8ea49003
Mauricio Araya <git@mauricioaraya.net>
2018-07-20 06:21:30 -0600

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# consulproject
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: consulproject
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
