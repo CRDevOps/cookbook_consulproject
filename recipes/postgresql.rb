9dbb0d5ccf53db8bd0c54a8bb82e6931a91cd1ea
Mauricio Araya <git@mauricioaraya.net>
2018-07-20 13:28:38 -0600

    ConsulProject: Chef cookbook

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -3,11 +3,24 @@
 # Recipe:: postgresql
 #
 
+# def chef_solo_search_installed?
+#   klass = ::Search.const_get('Helper')
+#   return klass.is_a?(Class)
+# rescue NameError
+#   return false
+# end
+
+# if Chef::Config[:solo] && !chef_solo_search_installed?
+#     Chef::Log.warn('This recipe uses search. Chef-Solo does not support search unless '\
+#                    'you install the chef-solo-search cookbook.')
+# else
+
+# end
+
 pgbag = data_bag('pg_admin')
-pgbag.first do |bag|
-    postgresql_server_install 'postgresql-server' do
-      password bag['password']
-      port 5433
-      action :create
-    end
-end
+
+Chef::Log.info 'mwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmw'
+Chef::Log.info pgbag
+Chef::Log.info 'mwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmw'
+Chef::Log.info pgbag.first
+Chef::Log.info 'mwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmwmw'
